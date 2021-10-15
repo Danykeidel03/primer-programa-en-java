535677e634da7ab8dd06e8daae3efe970460ecfd
Daniel <supertuxcar@gmail.com>
2021-10-15 12:05:18 +0200

    Cantante

--- prueba.java
+++ prueba.java
@@ -2,9 +2,10 @@ public class prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Daniel Fernandez Fernandez");
+
 		System.out.println("Leon 31-7-2003");
 		System.out.println("Calle Zurbano");
+		System.out.println("Kid Keo");
 
 }
 
